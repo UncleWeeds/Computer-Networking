67cc37593693be29fb1f6f415cebfefeaed59f7f
Abhi <danduabhiramsai@gmail.com>
2023-08-18 15:04:32 +0530

    Version2

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const fs = require('fs');
+
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+function logMessage(message) {
+  fs.appendFile('chat.log', message + '\n', (err) => {
+    if (err) {
+      console.error('Failed to write to log file:', err);
+    }
+  });
+}
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   ws.on('message', (message) => {
     console.log(`Received: ${message}`);
+    logMessage(`Received: ${message}`);
   });
 
   ws.send('Welcome to the chat server!');
