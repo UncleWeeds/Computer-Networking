08c049db0420cfe0d2e33fed94dc09b90b7da139
Abhi <danduabhiramsai@gmail.com>
2023-08-18 19:55:55 +0530

    Version3

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let clientCount = 0;
+let totalBytesReceived = 0;
+
 function logMessage(message) {
   fs.appendFile('chat.log', message + '\n', (err) => {
     if (err) {
@@ -16,16 +19,46 @@ function logMessage(message) {
   });
 }
 
+function reportNetworkUsage() {
+  console.log(`Number of clients: ${clientCount}`);
+  console.log(`Total bytes received: ${totalBytesReceived}`);
+}
+
+function broadcast(message, sender) {
+  wss.clients.forEach((client) => {
+    if (client !== sender && client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  clientCount++;
+
+  ws.name = 'Guest'; 
+
+  ws.send('Please enter your name:'); 
 
   ws.on('message', (message) => {
-    console.log(`Received: ${message}`);
-    logMessage(`Received: ${message}`);
-  });
+    if (ws.name === 'Guest') { 
+      ws.name = message; 
+      ws.send(`Welcome, ${ws.name}!`);
+      logMessage(`${ws.name} joined the chat.`);
+      return;
+    }
 
-  ws.send('Welcome to the chat server!');
+    const chatMessage = `${ws.name}: ${message}`; 
+    console.log(chatMessage);
+    logMessage(chatMessage);
+    totalBytesReceived += Buffer.from(message).length;
+  
+    broadcast(chatMessage);
+  });
 });
+
+setInterval(reportNetworkUsage, 10000);
 
 server.listen(3000, () => {
   console.log('Server started on http://localhost:3000');
