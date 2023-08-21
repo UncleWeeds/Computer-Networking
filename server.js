94dc6ba54fe0a5fa5b13ff2495b0691751c5c66b
Abhi <danduabhiramsai@gmail.com>
2023-08-21 07:54:11 +0530

    Version6

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const fs = require('fs');
+const crypto = require('crypto');
 
 
 const app = express();
@@ -10,6 +12,12 @@ const wss = new WebSocket.Server({ server });
 
 let clientCount = 0;
 let totalBytesReceived = 0;
+
+const users = {
+  'sam': crypto.createHash('sha256').update('password1').digest('hex'),
+  'john': crypto.createHash('sha256').update('password2').digest('hex'),
+  'Abhi': crypto.createHash('sha256').update('Abhi').digest('hex'),
+};
 
 function logMessage(message) {
   fs.appendFile('chat.log', message + '\n', (err) => {
@@ -34,25 +42,44 @@ function broadcast(message) {
 wss.on('connection', (ws) => {
   console.log('Client connected');
   clientCount++;
-  broadcast(`A new client has joined the chat room as Guest.`);
+  //broadcast(`A new client has joined the chat room.`);
 
   ws.name = 'Guest'; 
 
-  ws.send('Please enter your name:'); 
-
   ws.on('message', (message) => {
-    if (ws.name === 'Guest') { 
-      ws.name = message; 
-      ws.send(`Welcome, ${ws.name}!`);
-      logMessage(`${ws.name} joined the chat.`);
-      return;
+
+    if (Buffer.isBuffer(message)) {
+      message = message.toString(); 
     }
 
+    if (!ws.isAuthenticated) {
+      const [username, password] = message.split(':');
+      const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+  
+      if (users[username] && users[username] === hashedPassword) {
+        ws.isAuthenticated = true;
+        //ws.name = username;
+        ws.send('Authentication successful, please enter your name:');
+        return; 
+      } else {
+        ws.send('Authentication failed, please try again.');
+        ws.close();
+        return;
+      }
+    }
+
+    if (ws.name === 'Guest') { 
+      ws.name = message; 
+      //ws.send(`Welcome, ${ws.name}!`);
+      logMessage(`${ws.name} joined the chat.`);
+      broadcast(`${ws.name} joined the chat room.`);
+      return;
+    }
+   
     const chatMessage = `${ws.name}: ${message}`; 
     console.log(chatMessage);
     logMessage(chatMessage);
     totalBytesReceived += Buffer.from(message).length;
-  
     broadcast(chatMessage);
   });
 
